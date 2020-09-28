e45bf5c80d6effc08f8d435b389dd374139f066b
Manjari <manjimandara17>
2020-09-28 20:57:35 +0530

    Login activity added.

---
+++ app/src/main/java/com/damishs/ads_lk/ui/Login/Login.java
@@ -0,0 +1,16 @@
+package com.damishs.ads_lk.ui.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.damishs.ads_lk.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
