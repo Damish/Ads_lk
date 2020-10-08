24f09a825ad34945d10896d9ddb614d40a765a1f
kisal <kisalrandula123@gmail.com>
2020-10-08 10:14:32 +0530

    Created Advanced Search tab view, post ad UI changed : Kisal

--- app/src/main/java/com/damishs/ads_lk/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/damishs/ads_lk/ui/dashboard/DashboardFragment.java
@@ -11,12 +11,20 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.viewpager.widget.ViewPager;
 
 import com.damishs.ads_lk.R;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
+
+    private TabLayout mTabLayout;
+    private TabItem home,a1,a2,a3;
+    private ViewPager mPager;
+    private PageController pageController;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +38,38 @@ public class DashboardFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+
+
+
+
+        mTabLayout=root.findViewById(R.id.tablayout);
+        home=root.findViewById(R.id.home);
+        a1=root.findViewById(R.id.home1);
+        a2=root.findViewById(R.id.home2);
+        a3=root.findViewById(R.id.home3);
+        mPager = root.findViewById(R.id.viewpager);
+
+        pageController = new PageController(getActivity().getSupportFragmentManager(),mTabLayout.getTabCount());
+        mPager.setAdapter(pageController);
+        mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                mPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        mPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+
         return root;
     }
 }
---
+++ app/src/main/java/com/damishs/ads_lk/ui/dashboard/PropertiesAdvancedSearch.java
@@ -0,0 +1,21 @@
+package com.damishs.ads_lk.ui.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.damishs.ads_lk.R;
+
+
+public class PropertiesAdvancedSearch extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_properties_advanced_search, container, false);
+    }
+}
